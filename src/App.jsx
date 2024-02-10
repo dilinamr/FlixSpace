639b69955a101160350ef3b889829fa930db7de7
Dilin-am <dilinam91@gmail.com>
2024-02-11 00:15:42 +0530

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+
 import { fetchDataFromApi } from "./utils/api";
+import { getApiConfigaration } from "./store/homeSlice";
+
+getApiConfigaration;
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   useEffect(() => {
     apitesting();
   }, []);
   const apitesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfigaration(res));
     });
   };
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      APP
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/store/homeSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const homeSlice = createSlice({
+  name: "home",
+  initialState: {
+    url: {},
+    genres: {},
+  },
+  reducers: {
+    getApiConfigaration: (state, action) => {
+      state.url = action.payload;
+    },
+    getGenres: (state, action) => {
+      state.genres = action.payload;
+    },
+  },
+});
+
+export const { getApiConfigaration, getGenres } = homeSlice.actions;
+
+export default homeSlice.reducer;
