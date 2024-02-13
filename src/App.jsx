9a3b33c9ded0e2968876177fc897fc56b08b2432
Dilin-am <dilinam91@gmail.com>
2024-02-13 15:24:35 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,44 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfigaration } from "./store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 getApiConfigaration;
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
   useEffect(() => {
-    apitesting();
+    fetchApiConfig();
   }, []);
-  const apitesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
-      dispatch(getApiConfigaration(res));
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfigaration(url));
     });
   };
   return (
-    <div className="App">
-      APP
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
