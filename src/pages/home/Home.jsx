4f5b812109fd2792901577dfd4534c61002387a6
Dilin-am <dilinam91@gmail.com>
2024-02-22 02:15:52 +0530

    skelton carosel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import HeroBanner from "./heroBanner/HeroBanner";
 import "./style.scss";
+import Trending from "./trending/Trending";
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
     </div>
   );
 };
